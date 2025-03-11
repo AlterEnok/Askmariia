7d9e80573fe9c80a368879c996a7f4ef504f9f8b
Edem-Kestan <ed.kestan@gmail.com>
2025-03-11 19:24:34 +0200

    made the site

--- js/script.js
+++ js/script.js
@@ -109,3 +109,16 @@ const swiper = new Swiper('.reviews__slider', {
     spaceBetween: 20,
 });
 
+// SCROLL-UP
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    if (!scrollUp) return; // Защита от ошибок
+
+    if (window.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+};
+
+window.addEventListener('scroll', scrollUp);
